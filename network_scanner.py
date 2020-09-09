be4d9adae30908528ca16ae65b75bca21ff4a058
UA <mirxa.usama.mirxa@gmail.com>
2020-09-09 11:40:52 +0500

    Minor Improvements

--- network_scanner.py
+++ network_scanner.py
@@ -17,23 +17,59 @@ else:
 # User Defined Functions
 ########################################################################
 
+def nothing():
+
+    """Nothing ;)"""
+
+    pass
+
+def is_root():
+    
+    """This function will check whether the script was run as root or not"""
+
+    current_user_id = int(subprocess.check_output(["id","-u"]))
+    if current_user_id == 0:
+        return True
+    else:
+        return False
+
+def get_arguments():
+
+    """This function will get arguments from command line if there are any and return them to main function"""
+
+    parser = argparse.ArgumentParser(description="All arguments are optional")
+    parser.add_argument("-t","--target",dest="ip",help="IP or IP range to scan, all if not provided")
+    parser.add_argument("-s","--silent",dest="mute",help="Show less output",action="store_true")
+    options = parser.parse_args()
+    return options.ip,options.mute
+
+def get_ip_range():
+
+    """This function will craft the local IPv4 range string"""
+
+    ip = str(subprocess.check_output(["hostname","-I"]))
+    ip_range = re.search("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.",ip)
+    ip_range = str(ip_range[0])
+    ip_range += '1/24'
+    return ip_range
+
 def arp_scan(ip):
 
     """This function will craft ARP packets and then broadcast them on the network
        It will then return a list of dictionaries consisting of filtered responses"""
 
     # Crafting the ARP packet
-    print("[+] Crafting the ARP Packet/s to broadcast") if verbose else print(end="")
+    print("[+] Crafting the ARP Packet/s to broadcast") if not mute else nothing()
     broadcast_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_packet = scapy.ARP(pdst=ip)
     arp_broadcast_packet = broadcast_packet / arp_packet
     
     # Sending the packet on the network and recieving the response
-    print("[+] Sending the packet and recieving the responses in maximum 5 seconds") if verbose else print(end="")
+    print("[+] Sending the packet and recieving the responses in maximum 5 seconds")
     answered_packets_list = scapy.srp(arp_broadcast_packet,timeout=5,verbose=False)[0]
     
     # Filtering desired parts of responses and appending them into a list
-    print("[+] Parsing the Responses") if verbose else print(end="")
+    print("[+] Parsing the Responses") if not mute else nothing()
     filtered_responses = []
     for answer in answered_packets_list:
         response = answer[1] # [0] of answer contains our request and [1] contains the response
@@ -46,13 +82,12 @@ def arp_scan(ip):
     # Returning the filtered responses
     return filtered_responses
 
-
 def show_result(responses):
 
     """This function will show the result of scan on screen"""
 
-    print("[+] Displaying the responses in tabular form") if verbose else print(end="")
-    print() if verbose else print()
+    print("[+] Displaying the responses in tabular form") if not mute else nothing()
+    print()
     # Header
     print("      IP  \t|      MAC Address   ")
     print("-------------------------------------")
@@ -60,50 +95,31 @@ def show_result(responses):
     for response in responses:
         print(f" {response['IP']}\t| {response['MAC']}")
 
-def get_arguments():
-
-    """This function will get arguments from command line if there are any and return them to main function"""
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t","--target",dest="ip",help="IP or IP range to scan, all if not provided")
-    parser.add_argument("-v","--verbose",dest="vrb",help="Be Verbose? (0 for not, 1 for yes), 1 by default")
-    options = parser.parse_args()
-    return options.ip,options.vrb
-
-def get_ip_range():
-
-    """This function will craft the local IPv4 range string"""
-
-    ip = str(subprocess.check_output(["hostname","-I"]))
-    ip_range = re.search("[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.",ip)
-    ip_range = str(ip_range[0])
-    ip_range += '1/24'
-    return ip_range
-
 ########################################################################
 # The main function
 ########################################################################
 
-ip, verbose = get_arguments()
+# Parsing the arguments
+ip, mute = get_arguments()
 
-# Checking the arguments
-verbose = False if verbose == '0' else True
-ip = 'a' if not ip else ip
+# Checking for privileges
+if is_root():
+    nothing()
+else:
+    print("[!] Please run the script as root")
+    exit()
 
-# Getting IP range if IP was not provided
-if ip == 'a' or ip == 'A':
+# Getting local IP range if IP is not provided and providing feedback
+if not ip:
     ip = get_ip_range()
-
-# Feedback
-print(f"[>] IP (or IP Range) is set to {ip}")
-print(f"[>] Verbose mode is set to {'ON' if verbose else 'OFF'}")
+print(f"[>] IP (or IP Range) is set to {ip}") if not mute else nothing()
 
 # Starting the scan
-print() if verbose else print(end="")
-print(f"[*] Starting the process") if verbose else print(end="")
-print() if verbose else print(end="")
+print() if not mute else nothing()
+print(f"[+] Starting the scan") if not mute else nothing()
+print() if not mute else nothing()
 result = arp_scan(ip)
 
 # Displaying the result
-print() if verbose else print(end="")
+print() if not mute else nothing()
 show_result(result)
